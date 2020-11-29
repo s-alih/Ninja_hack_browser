7f7adbe7a903663168ec4430442670f0bc507dcf
salih <slh32064@gmail.com>
2020-11-29 13:47:28 +0530

    nav bar

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Button.css'
+import {Link} from 'react-router-dom'
+const  STYLES = ['btn--primary' ,'btn--outline']
+const  SIZES = ['btn--medium','btn--large']
+export const Button = ({children,type,onClick,buttonSize,buttonStyle}) =>{
+    
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    const checkSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+    return(
+        <Link to='sign-up' className='btn-mobile'>
+            <button type={type} onClick={onClick} className={`btn ${checkButtonStyle} ${checkSize}`}>
+                {children}
+            </button>
+        </Link>
+    ) 
+}
